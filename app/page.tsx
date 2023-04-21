bb32238af84fcffb43be0ba53e872f30ff7604e9
veer260 <57021124+veer260@users.noreply.github.com>
2023-04-21 11:36:57 +0530

    initial next setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      Hello Next!
+    </main>
+  );
+}
